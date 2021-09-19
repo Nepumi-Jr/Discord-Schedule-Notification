005ac7a72b2896bf490c62e5a56e5c74daed43f2
jirathon.chaisuk <jirathonchaisuk@kkumail.com>
2021-09-19 16:21:14 +0700

    Admin and AdminPass Check?!

--- src/cmdUtil.py
+++ src/cmdUtil.py
@@ -1,5 +1,4 @@
 def isAdmin(idUser) -> bool:
-    pass
     # TODO : คำสั่งนี้เป็นคำสั่งสำหรับเช็คว่าคนนี้เป็น Admin หรือเปล่า
     # * จะให้อ่านไฟล์ที่ชื่อว่า AdminUsers.txt ที่อยู่นอกโฟลเดอร์ src
     # * โดยไฟล์จะมีลักษณะแบบนี้
@@ -13,9 +12,17 @@ def isAdmin(idUser) -> bool:
     # ? ตัวอย่าง isAdmin(993224234) -> True
     # ? ตัวอย่าง isAdmin(142342345) -> False
 
+    file_path = "../AdminUsers.txt"
+    with open(file_path, 'r') as ID:
+        while ID:
+            line = ID.readline().strip()
+            if idUser == line:
+                return True
+            if line == "":
+                return False
+
 
 def regisAdmin(idUser, passText: str) -> int:
-    pass
     # TODO : คำสั่งนี้เป็นคำสั่งสำหรับการใส่ข้อมูลว่าคนนั้นเป็น admin
     # * จะให้อ่านไฟล์ที่ชื่อว่า AdminPassword.pass ที่อยู่นอกโฟลเดอร์ src
     # * โดยไฟล์จะมีลักษณะแบบนี้
@@ -32,3 +39,37 @@ def regisAdmin(idUser, passText: str) -> int:
     # ? ตัวอย่าง regisAdmin(993224234, "Hello world") -> 1 (บรรจุด้วย)
     # ? ตัวอย่าง regisAdmin(123222545, "123 cute cute") -> 2 (ไม่บรรจุ)
     # ? ตัวอย่าง regisAdmin(142342345, "456 brbrbr") -> 0 (ไม่บรรจุ)
+
+
+    # * เชคว่า passText ตรงกับ AdminPassword หรือไม่
+    file_path = "../AdminPassword.pass"
+    with open(file_path, 'r') as ID:
+        while ID:
+            line = ID.readline().strip()
+            if str(passText) == str(line):
+                passTextBool = True
+                break
+            if line == "":
+                passTextBool = False
+
+
+    # * เงื่อนไขการตรวจสอบ
+    if (isAdmin(idUser) and passTextBool):
+        return 2
+    elif passTextBool:
+        file_path = "../AdminUsers.txt"
+        f = open(file_path, "a")
+        f.write("\n"+idUser)
+        f.close()
+        return 1
+    else:
+        return 0
+            
+
+def main():
+    a = regisAdmin("1234956","Hello world")
+    print(a)
+
+
+if __name__ == "__main__":
+    main()
