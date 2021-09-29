0457b72f6a403eeb8cc8db7c071a9e5d08f4af4a
jirathon.chaisuk <jirathonchaisuk@kkumail.com>
2021-09-29 18:27:37 +0700

    timeDetection

--- src/cmdUtil.py
+++ src/cmdUtil.py
@@ -89,38 +89,17 @@ def regisChannel(idChannel: int) -> int:
 
 
 def timeDetection(content: str) -> tuple:
-    # TODO : ฟังก์ชั่นนี้จะทำการอ่าน string content และให้ส่งค่าออกมาเป็นเวลา
-    # * แนะนำให้เขียนเป็นฟังก์ชั่นในการหาตัวเลขจำนวนเต็มสองตัวใน string นี้ แล้วค่อยเช็คว่ามันเป็นเวลาที่ถูกไหม
-    # ? เงื่อนไขที่เป็นเวลาคือ ชั่วโมง จะอยู่ในช่วง [0,23] และ นาที จะอยู่ในช่วง [0,59]
-    # ? หากเป็นเวลา ให้คืนค่าเป็น Tuple สองตัว นั้นคือชม. และนาที
-    # ? หากไม่ใช่เวลา ให้คืนค่าเป็น Tuple ว่างเปล่า
-
-    # * ตัวอย่าง
-    "12 : 45"
-    # * Return -> (12,45)
-
-    "13.57 น."
-    # * Return -> (13,57)
-
-    "0 โมง 23 นาที"
-    # * Return -> (0,23)
-
-    "ตั้งตอน 11 นาฬิกา 22 นาที"
-    # * Return -> (11,22)
-
-    "23 โมง"
-    # ! Return -> ()
-
-    "1232"
-    # ! Return -> ()
-
-    "24.12"
-    # ! Return -> ()
-
-    "งึกๆงักๆ มันเป็นงึกๆงักๆ"
-    # ! Return -> ()
-    pass
-
+    content = content.replace(":"," ").replace("."," ")
+    word = content.split(" ")
+    time = []
+    for text in word:
+        if text.isdigit():
+            time.append(int(text))
+    if not(len(time) == 2):
+        return ()
+    if (time[0] >= 0 and time[0] <= 23) and (time[1] >= 0 and time[1] <= 59):
+        return tuple(time)
+    return ()
 
 def main():
     print(isUseChannel(123))
@@ -134,7 +113,8 @@ def main():
     print(isUseChannel(12))
     print(isUseChannel(1342))
     print(isUseChannel(23))
-
+    print(timeDetection("0 61 นาที"))
+    print(timeDetection("23:59 นาที"))
 
 if __name__ == "__main__":
     main()
